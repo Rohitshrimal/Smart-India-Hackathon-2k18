bf68ac371dde6cdb085f8c9a7911d038e516e924
Rohit <rohitshrimal11@gmail.com>
2018-03-30 14:13:46 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,78 @@
+var request = require("request");
+var lattitude = 18.520430;
+var longitude = 73.856744;
+
+function getData(callback) {
+    var options = {
+        method: 'GET',
+        url: 'https://api.data.gov.in/resource/fced6df9-a360-4e08-8ca0-f283fc74ce15',
+        qs: {
+            'api-key': '579b464db66ec23bdd000001f1c7774618144b456a41b7a967167f94',
+            format: 'json',
+            offset: '0',
+            limit: '3000'
+        }
+    };
+    request(options, function(error, response, body) {
+        if (error)
+            console.log(error);
+        else {
+            var body = JSON.parse(body)
+            callback(body.records);
+        }
+    });
+}
+
+function getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) {
+    var R = 6371; // Radius of the earth in km
+    var dLat = deg2rad(lat2 - lat1); // deg2rad below
+    var dLon = deg2rad(lon2 - lon1);
+    var a =
+        Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+        Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+        Math.sin(dLon / 2) * Math.sin(dLon / 2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    var d = R * c; // Distance in km
+    return d;
+}
+
+function deg2rad(deg) {
+    return deg * (Math.PI / 180)
+}
+
+function sortByKey(array, key) {
+    return array.sort(function(a, b) {
+        var x = a[key]; var y = b[key];
+        return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+    });
+}
+
+function getNearestBloodBanks(lat, long, callback) {
+    var nearestBloodBanks = [];
+
+    getData((bloodBankData) => {
+        bloodBankData.forEach(function(element) {
+            if (element._blood_component_available === 'YES') {
+                var distance = getDistanceFromLatLonInKm(lat,
+                    long, element._latitude, element._longitude
+                );
+//                console.log(distance);
+                if (distance <= 50) {
+                    var dataToAppend = {};
+                    dataToAppend.Address = element._address +
+                        '\r\n' + element._state
+                    dataToAppend.contact = element._contact_no.split();
+                    dataToAppend.distance = distance;
+                    console.log(dataToAppend, 'hello');
+                    nearestBloodBanks.push(dataToAppend);
+                }
+            }
+        });
+             callback(sortByKey(nearestBloodBanks,'distance'));
+    });
+
+}
+
+getNearestBloodBanks(lattitude, longitude, (data) => {
+    console.log(data);
+});
